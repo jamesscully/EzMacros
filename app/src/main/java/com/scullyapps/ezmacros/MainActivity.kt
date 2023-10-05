9b59f2e502cd43d392995c981892c045c3521ba1
James Scully <james.will.scully@gmail.com>
2023-10-05 03:21:15 +0100

    First commit

--- app/src/main/java/com/scullyapps/ezmacros/MainActivity.kt
+++ app/src/main/java/com/scullyapps/ezmacros/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Test
+        // Test a
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
