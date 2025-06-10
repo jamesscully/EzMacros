67e6624035e7ab0f324a5c11e62cc28332c7de53
James Scully <james.will.scully@gmail.com>
2025-06-10 21:01:27 +0100

    Enter weight is now stored in VM with flow, no unit transfer yet

--- app/src/main/java/com/scullyapps/ezmacros/ui/weight/EnterWeightFragment.kt
+++ app/src/main/java/com/scullyapps/ezmacros/ui/weight/EnterWeightFragment.kt
@@ -2,13 +2,16 @@ package com.scullyapps.ezmacros.ui.weight
 
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.scullyapps.ezmacros.R
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import com.scullyapps.ezmacros.databinding.FragmentEnterWeightBinding
 
+val TAG = "EnterWeightFragment"
 class EnterWeightFragment : Fragment() {
 
     companion object {
@@ -17,16 +20,49 @@ class EnterWeightFragment : Fragment() {
 
     private val viewModel: EnterWeightViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _binding: FragmentEnterWeightBinding? = null
+    private val b get() = _binding!!
 
-        val binding = FragmentEnterWeightBinding.inflate(layoutInflater)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_enter_weight, container, false)
+        _binding = FragmentEnterWeightBinding.inflate(inflater, container, false)
+        val view = b.root
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Weight limit in KG
+        val WEIGHT_LIMIT = 200
+
+        b.lblWeightUnitDisplay.text = "kg"
+        b.inputWeightNumber.setText(viewModel.uiState.value.weight.toString())
+        b.inputWeightNumber.addTextChangedListener { text ->
+
+            val number : Double = text.toString().toDoubleOrNull() ?: 0.0
+
+            if(number > WEIGHT_LIMIT) {
+                b.inputWeightNumber.setText("$WEIGHT_LIMIT")
+                b.inputWeightNumber.setSelection(WEIGHT_LIMIT.toString().length)
+            }
+
+            viewModel.uiState.value.weight = number
+
+            Log.i(TAG, "onCreateView: Text is now $text")
+        }
+
+        b.btnSubmitWeight.setOnClickListener {
+            Toast.makeText(context, "Number is: ${viewModel.uiState.value.weight}", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
     }
 }
--- app/src/main/java/com/scullyapps/ezmacros/ui/weight/EnterWeightViewModel.kt
+++ app/src/main/java/com/scullyapps/ezmacros/ui/weight/EnterWeightViewModel.kt
@@ -1,7 +1,21 @@
 package com.scullyapps.ezmacros.ui.weight
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class EnterWeightFragmentUiState(
+    var weight : Double = 0.0,
+    var unitType : Boolean = false
+)
 
 class EnterWeightViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+
+    private val _uiState = MutableStateFlow(EnterWeightFragmentUiState())
+    val uiState : StateFlow<EnterWeightFragmentUiState> = _uiState.asStateFlow()
+
 }
